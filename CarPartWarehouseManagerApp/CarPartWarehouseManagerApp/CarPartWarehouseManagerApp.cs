2f18bd973c57dcce40f2e3894f01593387f05ea1
MWolf538329 <538329@student.fontys.nl>
2024-11-06 11:24:25 +0100

    Manager App Setup
    Still debating if this is going to work

---
+++ CarPartWarehouseManagerApp/CarPartWarehouseManagerApp/CarPartWarehouseManagerApp.cs
@@ -0,0 +1,15 @@
+namespace CarPartWarehouseManagerApp
+{
+    public partial class CarPartWarehouseManagerApp : Form
+    {
+        public CarPartWarehouseManagerApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CategoryOverview_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
