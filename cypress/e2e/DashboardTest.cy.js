9c8bb377eacc59ceb608e163503618b46f1b7e17
MWolf538329 <538329@student.fontys.nl>
2025-01-08 14:02:35 +0100

    https -> http

--- cypress/e2e/DashboardTest.cy.js
+++ cypress/e2e/DashboardTest.cy.js
@@ -2,14 +2,14 @@
 
 describe('Visits Localhost and Retrieves Category, Subcategory and Product Data', () => {
   it('Connects to Localhost -> Opens Category Accordion -> Opens Subcategory Accordion -> Checks ProductCard to be Green', () => {
-    cy.visit('https://localhost')
+    cy.visit('http://localhost')
     cy.get('.cypressCategoryAccordion').contains("E2E Test Category").click()
     cy.get('.cypressSubcategoryAccordion').contains("E2E Test Subcategory").click()
     cy.get('.cypressProductCard').first().should('have.class', 'bg-green-300')
   })
 
   it('Connects to Localhost -> Opens Category Accordion -> Opens Subcategory Accordion -> Checks ProductCard to be Red', () => {
-    cy.visit('https://localhost')
+    cy.visit('http://localhost')
     cy.get('.cypressCategoryAccordion').contains("E2E Test Category").click()
     cy.get('.cypressSubcategoryAccordion').contains("E2E Test Subcategory").click()
     cy.get('.cypressProductCard').last().should('have.class', 'bg-red-300')
